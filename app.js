a093aac2b59545ee1d74271c247b6fe258916386
Bukunjom <bom1za1210@gmail.com>
2018-02-08 04:42:04 +0000

    FB api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express         = require("express"),
     mongoose        = require("mongoose"),
     passport        = require("passport"),
     LocalStrategy   = require("passport-local"),
+    FacebookStrategy= require("passport-facebook").Strategy,
     flash           = require("connect-flash"),
     session         = require("express-session"),
     methodOverride  = require("method-override"),
@@ -38,14 +39,29 @@ app.locals.moment = require("moment");
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){ done(null, user) });
+passport.deserializeUser(function(user, done){ done(null, user) });
+passport.use(new FacebookStrategy({
+    clientID: "2030077057205209",
+    clientSecret: "2ef9bf727225c99d4df0fe738fe997b6",
+    callbackURL: "https://myportfolio-bukunjom.c9users.io/auth/facebook/callback",
+    profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
+  },
+  function(accessToken, refreshToken, profile, done){
+      profile.accessToken = accessToken;
+      done(null, profile)
+  }
+));
 
 app.use(function(req, res, next){
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
+});
+
+app.get("/google151fffa848e804ed.html", function(req, res){
+   res.send("google-site-verification: google151fffa848e804ed.html"); 
 });
 
 app.use("/", indexRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,50 @@
 var express = require("express"),
     router  = express.Router(),
     passport = require("passport"),
+    async   = require("async"),
+    nodeMailer = require("nodemailer"),
+    crypto  = require("crypto"),
     Campground = require("../models/campground"),
     User  = require("../models/user"),
+    multer = require("multer"),
+    request = require('request'),
     middleware = require("../middleware");
+    
+const userFieldSet = 'name, link, is_verified, picture';
+const pageFieldSet = 'name, category, link, picture, is_verified';
+    
+var smtpTransport = nodeMailer.createTransport({
+               service: "Gmail",
+               auth: {
+                       user: "patiparnair@gmail.com",
+                       pass: "15111994",
+               }
+            });
+
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: "dlaelxhbp", 
+  api_key: "492524649579695", 
+  api_secret: "c1hU9DtqLmmPHsvn0k6kqZBrZKc"
+});
 
 //LANDING - first page
 router.get("/", function(req,res){
-    res.send("Hello");
+    res.send("HELLO WORLD");
 });
 
 //REGISTER - show form to create new user
@@ -16,25 +53,28 @@ router.get("/register", function(req, res){
 });
 
 //SIGNUP - add new user to db
-router.post("/register",function(req, res){
-    var newUser = new User({
-        username: req.body.username,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        avatar: req.body.avatar
-    });
-    if(req.body.adminCode === "amnotgonnatellu"){
-        newUser.isAdmin = true;
-    };
-    User.register(newUser, req.body.password, function(err,user){
-        if(err){
-            req.flash("error", err.message);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Successfully, Sign Up! Nice to meet you " + user.username);
-            res.redirect("/campgrounds");
+router.post("/register", upload.single("image"), function(req, res){
+    cloudinary.uploader.upload(req.file.path, function(result) {
+        var avatar = result.secure_url;
+        var newUser = new User({
+            username: req.body.username,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            avatar: avatar
+        });
+        if(req.body.adminCode === "amnotgonnatellu"){
+            newUser.isAdmin = true;
+        };
+        User.register(newUser, req.body.password, function(err,user){
+            if(err){
+                req.flash("error", err.message);
+                return res.render("register");
+            }
+            passport.authenticate("local")(req, res, function(){
+                req.flash("success", "Successfully, Sign Up! Nice to meet you " + user.username);
+                res.redirect("/campgrounds");
+            });
         });
     });
 });
@@ -42,6 +82,50 @@ router.post("/register",function(req, res){
 //LOGIN - show form to sign in
 router.get("/login", function(req, res){
    res.render("login"); 
+});
+
+router.get('/auth/facebook', passport.authenticate('facebook', {
+    failureRedirect: '/login',
+    scope:['email']
+}));
+router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req,res){
+        cloudinary.uploader.upload("https://graph.facebook.com/"+req.user.id+"/picture?type=large", function(result) {
+            var avatar = result.secure_url;
+            var newUser = {
+                firstname: req.user._json.first_name,
+                lastname: req.user._json.last_name,
+                email: req.user._json.email,
+                avatar: avatar,
+                publicId: result.public_id
+            };
+            res.render("fbLogin", {user: newUser})
+        });
+    }
+);
+
+router.post("/fbRegister", function(req, res){
+    var newUser = new User({
+        username: req.body.username,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        avatar: req.body.image
+    });
+    User.register(newUser, req.body.password, function(err,user){
+        if(err){
+            req.logout();
+            req.flash("error", err.message);
+            cloudinary.v2.uploader.destroy(req.body.publicId, 
+                function(error, result) {console.log(result) });
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Successfully, Sign Up! Nice to meet you " + user.username);
+            res.redirect("/campgrounds");
+        });
+    });
 });
 
 //SIGNIN - matching data and user db
@@ -59,6 +143,106 @@ router.get("/logout", function(req, res){
    req.logout();
    req.flash("success", "LOG YOU OUT!")
    res.redirect("/campgrounds");
+});
+
+//FORGOT PASSWORD
+router.get("/forgot", function(req, res){
+    res.render("forgot");
+});
+
+router.post("/forgot", function(req, res, next){
+    async.waterfall([
+        function(done){
+           crypto.randomBytes(20, function(err, buff){
+               var token = buff.toString("hex");
+               done(err, token);
+           }); 
+        },
+        function(token, done){
+            User.findOne({ email: req.body.email }, function(err, user){
+                if(!user){
+                    req.flash("error", "No account with that email address exists.");
+                    return res.redirect("/forgot");
+                }
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+                user.save(function(err){
+                   done(err, token, user); 
+                });
+            });
+        },
+        function(token, user, done){
+            var mailOptions = {
+                to: user.email,
+                from: "patiparnair@gmail.com",
+                subject: "YelpCamp Password Reset",
+                text: "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+                    "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+                    "http://" + req.headers.host + "/reset/" + token + "\n\n" +
+                    "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+            };
+            smtpTransport.sendMail(mailOptions, function(err){
+                console.log("mail sent");
+                req.flash("success", "An email have been sent to " + user.email + " with further instuctions.");
+                done(err, "done");
+            });
+        }
+    ], function(err){
+      if(err) return next(err);
+      res.redirect("/forgot");
+    });
+});
+
+router.get("/reset/:token", function(req, res){
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user){
+        if(!user){
+            req.flash("error", "Password reset token is invalid or has expired.");
+            return res.redirect("/forgot");
+        }
+        res.render("reset", { token: req.params.token });
+    });
+});
+
+router.post("/reset/:token", function(req, res){
+    async.waterfall([
+        function(done){
+            User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user){
+                if(!user){
+                    req.flash("error", "Password reset token is invalid or has expired.");
+                    return res.redirect("back");
+                }
+                if(req.body.password === req.body.confirm){
+                    user.setPassword(req.body.password, function(err){
+                       user.resetPasswordToken = undefined;
+                       user.resetPasswordExpires = undefined;
+                       user.save(function(err){
+                          req.logIn(user, function(err){
+                              done(err, user);
+                          });
+                       });
+                    });
+                } else {
+                    req.flash("error", "Passwords do not match.");
+                    return res.redirect("back");
+                }
+            });
+        },
+        function(user, done){
+            var mailOptions = {
+                to: user.email,
+                from: "patiparnair@gmail.com",
+                subject: "Your password have been changed",
+                text: "Hello,\n\n" +
+                    "This is a confirmation that the password for your account " + user.email + " has just been changed.\n"
+            };
+            smtpTransport.sendMail(mailOptions, function(err){
+                req.flash("success", "Successfully changed your password!!!");
+                done(err);
+            });
+        }
+    ], function(err){
+        res.redirect("/campgrounds");
+    });
 });
 
 //USER PROFILE - show information of user
